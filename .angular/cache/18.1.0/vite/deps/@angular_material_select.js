fd4572584dd0cd8bce1d681d56b45ea88b1944a5
Suchithra B <suchithra2025@gmail.com>
2025-02-03 23:17:48 +0530

    First Commit
    03-02-2025

---
+++ .angular/cache/18.1.0/vite/deps/@angular_material_select.js
@@ -0,0 +1,55 @@
+import {
+  MAT_SELECT_CONFIG,
+  MAT_SELECT_SCROLL_STRATEGY,
+  MAT_SELECT_SCROLL_STRATEGY_PROVIDER,
+  MAT_SELECT_SCROLL_STRATEGY_PROVIDER_FACTORY,
+  MAT_SELECT_TRIGGER,
+  MatSelect,
+  MatSelectChange,
+  MatSelectModule,
+  MatSelectTrigger,
+  matSelectAnimations
+} from "./chunk-ACNKIBD5.js";
+import "./chunk-GMVU4IY2.js";
+import "./chunk-BMCMDETF.js";
+import "./chunk-7YXSY363.js";
+import "./chunk-7CAVALHP.js";
+import "./chunk-M235WWNV.js";
+import {
+  MatError,
+  MatFormField,
+  MatHint,
+  MatLabel,
+  MatPrefix,
+  MatSuffix
+} from "./chunk-7P4HUR5I.js";
+import "./chunk-X6ODEJK2.js";
+import "./chunk-OKP347V7.js";
+import {
+  MatOptgroup,
+  MatOption
+} from "./chunk-GGTRGQHN.js";
+import "./chunk-NMO3Y5XW.js";
+import "./chunk-HH3A3BNW.js";
+import "./chunk-WDMUDEB6.js";
+export {
+  MAT_SELECT_CONFIG,
+  MAT_SELECT_SCROLL_STRATEGY,
+  MAT_SELECT_SCROLL_STRATEGY_PROVIDER,
+  MAT_SELECT_SCROLL_STRATEGY_PROVIDER_FACTORY,
+  MAT_SELECT_TRIGGER,
+  MatError,
+  MatFormField,
+  MatHint,
+  MatLabel,
+  MatOptgroup,
+  MatOption,
+  MatPrefix,
+  MatSelect,
+  MatSelectChange,
+  MatSelectModule,
+  MatSelectTrigger,
+  MatSuffix,
+  matSelectAnimations
+};
+//# sourceMappingURL=@angular_material_select.js.map
